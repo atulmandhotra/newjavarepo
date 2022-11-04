7e4f916c2b6b0b5f701b87d19bf0a08e151bd52e
Atul <atulmandhotra9@gmail.com>
2022-11-05 03:28:39 +0530

    changes hello

--- test/src/main/java/org/check/test2/prjNewJunit/HelloWorld.java
+++ test/src/main/java/org/check/test2/prjNewJunit/HelloWorld.java
@@ -1,10 +1,8 @@
 package org.check.test2.prjNewJunit;
 
-import java.util.logging.Logger;
-
 public class HelloWorld {
 
     public static void main(String... args) {
-        Logger logger=Logger.getLogger(HelloWorld.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+       System.out.println("hello");
+    }
 }
